c902b17df461fc31fe71cd3c6277a64af7c620b1
adheliaissabel <h1101241001@student.untan.ac.id>
2026-03-27 18:57:00 +0700

    fix: classroom scopes

--- apps/backend/src/auth.ts
+++ apps/backend/src/auth.ts
@@ -22,7 +22,7 @@ export function getAuthUrl(
     access_type: "offline",
     scope: [
       "https://www.googleapis.com/auth/classroom.courses.readonly",
-      "https://www.googleapis.com/auth/classroom.coursework.me.readonly",
+      "https://www.googleapis.com/auth/classroom.coursework.me",
       "https://www.googleapis.com/auth/classroom.student-submissions.me.readonly",
       "https://www.googleapis.com/auth/userinfo.email",
       "https://www.googleapis.com/auth/userinfo.profile",
